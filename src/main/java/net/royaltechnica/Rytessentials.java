f3df68351b92838b851ea9d628e6f5748c679839
BuildTools <unconfigured@null.spigotmc.org>
2021-05-09 20:11:53 +0200

    Initial commit

---
+++ src/main/java/net/royaltechnica/Rytessentials.java
@@ -0,0 +1,19 @@
+package net.royaltechnica;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Rytessentials extends JavaPlugin {
+
+    String prefix = "§7[§5Royal §aTech§enica§7] §7";
+
+    @Override
+    public void onEnable() {
+        Bukkit.getConsoleSender().sendMessage(prefix + "§aPlugin successfully enabled");
+
+    }
+    @Override
+    public void onDisable() {
+        Bukkit.getConsoleSender().sendMessage(prefix + "§cPlugin successfully disabled");
+    }
+}
